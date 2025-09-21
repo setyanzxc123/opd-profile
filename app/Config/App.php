2300b64b2c3f023f02cd3cfa6e36d8bcf07d8750
setyan <setyanzxc@gmail.com>
2025-09-21 16:07:50 +0800

    Enable clean URLs by clearing indexPage

--- app/Config/App.php
+++ app/Config/App.php
@@ -40,7 +40,7 @@ class App extends BaseConfig
      * something else. If you have configured your web server to remove this file
      * from your site URIs, set this variable to an empty string.
      */
-    public string $indexPage = 'index.php';
+    public string $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
@@ -200,3 +200,4 @@ class App extends BaseConfig
      */
     public bool $CSPEnabled = false;
 }
+
